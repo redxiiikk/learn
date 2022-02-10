b11686859f783a1391f9aa447c31b48a042357b3
Kai.He <kai.he.0811@qq.com>
2022-02-11 00:21:08 +0800

    fix: use compose down command

--- flink/deploy/stop.sh
+++ flink/deploy/stop.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
 # clear resources
-nerdctl container stop taskmanager-1
-nerdctl container stop taskmanager-2
-nerdctl container stop jobmanager
-nerdctl container stop redis
-
-nerdctl container rm taskmanager-1
-nerdctl container rm taskmanager-2
-nerdctl container rm jobmanager
-nerdctl container rm redis
-
-nerdctl network rm flink-network
+nerdctl compose -p flink -f ./compose.yml down
