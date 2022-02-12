98e66cc7dddc9cf08b17dd29c357e16481021e4e
Kai.He <kai.he.0811@qq.com>
2022-02-12 21:55:32 +0800

    feat: source and sink with kafka in word count job

--- flink/word-count/src/main/java/com/github/redxiiikk/learn/flink/wordcount/WordCountApplication.java
+++ flink/word-count/src/main/java/com/github/redxiiikk/learn/flink/wordcount/WordCountApplication.java
@@ -1,30 +1,57 @@
 package com.github.redxiiikk.learn.flink.wordcount;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.common.serialization.SerializationSchema;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.connector.base.DeliveryGuarantee;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.redis.RedisSink;
-import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
-import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
-import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
-import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
 import org.apache.flink.util.Collector;
 
 public final class WordCountApplication {
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        FlinkJedisPoolConfig conf = new FlinkJedisPoolConfig.Builder().setHost("redis").setPort(6379).build();
-        DataStream<String> text = executionEnvironment.fromElements("Hello Flink, This Is My First Stream App");
+        DataStream<String> text = executionEnvironment.fromSource(
+                generatorSource(), WatermarkStrategy.noWatermarks(), "word-topic"
+        );
 
         text.flatMap(new Tokenizer())
-                .keyBy(new WordCountKeySelector())
-                .sum(1)
-                .addSink(new RedisSink<>(conf, new WordCountRedisMapper()));
+                .keyBy(event -> event.f0)
+                .reduce((value1, value2) -> new Tuple2<>(value1.f0, value1.f1 + value2.f1))
+                .sinkTo(generatorKafkaSink()).name("word-count-topic");
 
         executionEnvironment.execute("WordCountApplication");
+    }
+
+    private static KafkaSource<String> generatorSource() {
+        return KafkaSource.<String>builder()
+                .setBootstrapServers("kafka:19092")
+                .setGroupId("word-count-consumer")
+                .setTopics("word")
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
+    }
+
+    private static KafkaSink<Tuple2<String, Integer>> generatorKafkaSink() {
+        KafkaRecordSerializationSchema<Tuple2<String, Integer>> recordSerializer
+                = KafkaRecordSerializationSchema.builder()
+                .setTopic("word_count")
+                .setValueSerializationSchema(new JsonSerializationSchema<Tuple2<String, Integer>>())
+                .build();
+
+        return KafkaSink.<Tuple2<String, Integer>>builder()
+                .setBootstrapServers("kafka:19092")
+                .setRecordSerializer(recordSerializer)
+                .setDeliverGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+                .build();
     }
 
     public static final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
@@ -43,28 +70,13 @@ public final class WordCountApplication {
         }
     }
 
-    public static final class WordCountKeySelector implements KeySelector<Tuple2<String, Integer>, String> {
-        @Override
-        public String getKey(Tuple2<String, Integer> stringIntegerTuple2) {
-            return stringIntegerTuple2.getField(0);
-        }
-    }
+    public static final class JsonSerializationSchema<T> implements SerializationSchema<T> {
+        private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    public static class WordCountRedisMapper implements RedisMapper<Tuple2<String, Integer>> {
-
+        @SneakyThrows
         @Override
-        public RedisCommandDescription getCommandDescription() {
-            return new RedisCommandDescription(RedisCommand.HSET, "WordCountApplicationResult");
-        }
-
-        @Override
-        public String getKeyFromData(Tuple2<String, Integer> data) {
-            return data.f0;
-        }
-
-        @Override
-        public String getValueFromData(Tuple2<String, Integer> data) {
-            return data.f1.toString();
+        public byte[] serialize(T element) {
+            return OBJECT_MAPPER.writeValueAsBytes(element);
         }
     }
 }
