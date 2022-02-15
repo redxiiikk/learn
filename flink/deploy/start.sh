a922e1a73ffbab949678afa6f3a30467ca1ca5c3
Kai.He <kai.he.0811@qq.com>
2022-02-15 11:15:38 +0800

    feat: custom bulid flink image to enable flink queryable state

--- flink/deploy/start.sh
+++ flink/deploy/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+nerdctl build -f ./flink/Dockerfile --tag 'redxiiikk/flink:latest' ./flink
 nerdctl compose -p flink -f ./compose.yml up -d
 
-# nerdctl container start kowl kafka-exporter
+# nerdctl container start kowl
