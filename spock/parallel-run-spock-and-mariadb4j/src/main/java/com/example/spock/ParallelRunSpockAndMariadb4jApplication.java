d192c26665babc9d9b241e8556db18c1af27df37
redxiiikk <redxiiikk@outlook.com>
2022-06-23 22:44:07 +0800

    chore: 创建一个工程准备测试 spock 与 mariaDB4j 的集成测试

---
+++ spock/parallel-run-spock-and-mariadb4j/src/main/java/com/example/spock/ParallelRunSpockAndMariadb4jApplication.java
@@ -0,0 +1,13 @@
+package com.example.spock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelRunSpockAndMariadb4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParallelRunSpockAndMariadb4jApplication.class, args);
+    }
+
+}
---
+++ spock/parallel-run-spock-and-mariadb4j/src/test/java/com/example/spock/ParallelRunSpockAndMariadb4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelRunSpockAndMariadb4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
