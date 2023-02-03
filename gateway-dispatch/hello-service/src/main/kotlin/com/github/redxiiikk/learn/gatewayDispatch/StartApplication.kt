c475c6e025066173a11346d97b88355aaa801c2a
redxiiikkTW <116648160+redxiiikkTW@users.noreply.github.com>
2023-02-03 16:00:05 +0800

    chore: deploy nacos and register hello service

--- gateway-dispatch/hello-service/src/main/kotlin/com/github/redxiiikk/learn/gatewayDispatch/StartApplication.kt
+++ gateway-dispatch/hello-service/src/main/kotlin/com/github/redxiiikk/learn/gatewayDispatch/StartApplication.kt
@@ -2,9 +2,27 @@ package com.github.redxiiikk.learn.gatewayDispatch
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@EnableDiscoveryClient
 @SpringBootApplication
-class DemoApplication
+open class StartApplication
+
 fun main(args: Array<String>) {
-    runApplication<DemoApplication>(*args)
+    runApplication<StartApplication>(*args)
+}
+
+
+@RestController
+@RequestMapping("/hello")
+open class HelloController {
+    @GetMapping
+    fun hello() = "Hello, World"
+
+    @GetMapping("/{name}")
+    fun hello(@PathVariable name: String) = "Hello, $name"
 }
