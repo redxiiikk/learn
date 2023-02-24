1d48252b1addc86b854a198f554c5408cf599a52
redxiiikkTW <116648160+redxiiikkTW@users.noreply.github.com>
2023-02-24 12:25:47 +0800

    chore: clean image and container when restart service

--- gateway-dispatch/run.sh
+++ gateway-dispatch/run.sh
@@ -6,14 +6,18 @@ podman machine init -m 4069 --cpus 4 -v $HOME:$HOME --now
 # 2.1 deploy register service
 cd deploy && podman-compose up -d nacos-mysql nacos-register-service && cd ..
 # 2.2 deploy gateway service
-./gradlew gateway-service:clean  gateway-service:bootJar
+podman container stop gateway-service
+podman container rm gateway-service
 podman image rm docker.io/redxiiikk/learn-gateway-service:1.0.0
+./gradlew gateway-service:clean  gateway-service:bootJar
 cd gateway-service && podman build -t docker.io/redxiiikk/learn-gateway-service:1.0.0 . && cd ..
 cd ./deploy && podman-compose up -d gateway-service && cd ..
 
 # 2.3 deploy hello service
-./gradlew hello-service:clean  hello-service:bootJar
+podman container stop hello-service-baseline hello-service-dev
+podman container rm hello-service-baseline hello-service-dev
 podman image rm docker.io/redxiiikk/learn-hello-service:1.0.0
+./gradlew hello-service:clean  hello-service:bootJar
 cd hello-service && podman build -t docker.io/redxiiikk/learn-hello-service:1.0.0 . && cd ..
 cd ./deploy && podman-compose up -d hello-service-baseline hello-service-dev && cd ..
 
