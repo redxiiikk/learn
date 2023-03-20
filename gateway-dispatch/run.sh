4e1f8c34a446ea5b146f4c98d73f629ff075910d
redxiiikkTW <116648160+redxiiikkTW@users.noreply.github.com>
2023-03-20 16:46:35 +0800

    config disk size & image path when init podman machine

--- gateway-dispatch/run.sh
+++ gateway-dispatch/run.sh
@@ -1,5 +1,5 @@
 # 1. create virtual machine and start run container runtime
-podman machine init -m 4069 --cpus 4 -v $HOME:$HOME --now
+podman machine init --disk-size 50 -m 4069 --cpus 4 -v $HOME:$HOME --image-path "$HOME/Workspace/Library/QEMU/fedora-coreos-37.20230303.2.0-qemu.aarch64.qcow2.xz"
 podman machine start podman-machine-default
 
 # 2. start deploy all service
