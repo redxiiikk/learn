3d46b4d75eda3ed99dd7dc52390639acc5941105
redxiiikkTW <116648160+redxiiikkTW@users.noreply.github.com>
2023-03-21 13:21:25 +0800

    adjust ServiceInstanceListSupplier config

--- gateway-dispatch/gateway-service/src/main/kotlin/com/github/redxiiikk/learn/gatewayDispatch/StartApplication.kt
+++ gateway-dispatch/gateway-service/src/main/kotlin/com/github/redxiiikk/learn/gatewayDispatch/StartApplication.kt
@@ -1,9 +1,10 @@
 package com.github.redxiiikk.learn.gatewayDispatch
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.annotation.Bean
@@ -17,16 +18,14 @@ fun main(args: Array<String>) {
     runApplication<StartApplication>(*args)
 }
 
-
-@Configuration
-@LoadBalancerClients
-open class GatewayConfiguration {
+@Configuration(proxyBeanMethods = false)
+open class LoadBalancerConfig {
     @Bean
-    open fun envColorServiceInstanceListSupplier(context: ConfigurableApplicationContext): ServiceInstanceListSupplier {
-        return ServiceInstanceListSupplier.builder()
+    @ConditionalOnBean(ReactiveDiscoveryClient::class)
+    open fun discoveryClientServiceInstanceListSupplier(context: ConfigurableApplicationContext): ServiceInstanceListSupplier =
+        ServiceInstanceListSupplier.builder()
             .withDiscoveryClient()
-            .withCaching()
             .withHints()
+            .withCaching()
             .build(context)
-    }
 }
