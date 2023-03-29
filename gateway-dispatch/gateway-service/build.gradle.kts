ed6253a46fdfde13133418808f56d19483fa1168
redxiiikkTW <116648160+redxiiikkTW@users.noreply.github.com>
2023-03-29 17:28:12 +0800

    extract traffic isolation logic to another project(https://github.com/redxiiikk/spring-cloud-gateway-extension)

--- gateway-dispatch/gateway-service/build.gradle.kts
+++ gateway-dispatch/gateway-service/build.gradle.kts
@@ -6,4 +6,6 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-loadbalancer")
     implementation("com.alibaba.cloud:spring-cloud-starter-alibaba-nacos-discovery")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    implementation("com.github.redxiiikk.spring.cloud:spring-cloud-starter-gateway-loadbalancer-extension:0.0.1-SNAPSHOT")
 }
--- gateway-dispatch/gateway-service/src/main/kotlin/com/github/redxiiikk/learn/gatewayDispatch/StartApplication.kt
+++ gateway-dispatch/gateway-service/src/main/kotlin/com/github/redxiiikk/learn/gatewayDispatch/StartApplication.kt
@@ -1,5 +1,6 @@
 package com.github.redxiiikk.learn.gatewayDispatch
 
+import com.github.redxiiikk.spring.cloud.gateway.loadbalancer.EnableLoadBalancerIsolation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
@@ -8,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigurationProperties
+@EnableLoadBalancerIsolation
 open class StartApplication
 
 fun main(args: Array<String>) {
