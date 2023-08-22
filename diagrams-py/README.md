8613493979e5cdec6ccd716f0906737b44207a16
redxiiikk <72877527+redxiiikk@users.noreply.github.com>
2023-08-22 22:26:40 +0800

    delete unupdate sub project

--- diagrams-py/README.md
+++
@@ -1,3 +0,0 @@
-# Diagrams as Code
-
-使用 Python 脚本进行画图
--- diagrams-py/run.sh
+++
@@ -1,9 +0,0 @@
-brew install graphviz
-
-if [[ ! -d ".venv" ]]; then
-    python3 -m venv .venv
-fi
-
-source .venv/bin/activate
-
-python -m pip install --upgrade pip
