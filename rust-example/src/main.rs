785d0f9c3cf3d55073520644ea499845bfd7cfa2
redxiiikk <72877527+redxiiikk@users.noreply.github.com>
2023-10-06 15:25:13 +0800

    simple input and print for hello world

--- rust-example/src/main.rs
+++ rust-example/src/main.rs
@@ -1,3 +1,11 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    // read input from stdin
+    println!("Enter your name: ");
+    let mut input = String::new();
+    
+    stdin().read_line(&mut input).unwrap();
+    
+    println!("Hello, world! {input}");
 }
