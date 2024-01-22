00292db38ace5a3675f2edf8d3adc1fa9bb391c5
redxiiikkTW <116648160+redxiiikkTW@users.noreply.github.com>
2024-01-22 13:40:19 +0800

    stash

--- nuxt-starter/nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-    devtools: {enabled: false},
-    modules: ['@element-plus/nuxt', '@nuxtjs/supabase'],
-    elementPlus: {
-        importStyle: 'scss',
-    },
-    css: ["~/assets/css/main.scss"]
-});
